281e4631d3c32d937bac3c9ef36bfaeba0bad802
German Gomez <ggomez1728@gmail.com>
2016-03-11 20:21:00 -0430

    trabajo final

--- buscadorLibrosTablas/buscadorLibrosTablas/DetalleViewController.swift
+++ buscadorLibrosTablas/buscadorLibrosTablas/DetalleViewController.swift
@@ -22,38 +22,36 @@ class DetalleViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var imageURL: UIImageView!
     
     
-    @IBOutlet weak var search_txt: UISearchBar!
     var response : String? = nil
 
     
-    var esBusqueda: Bool = false
-    var ISBN: String?
     var titulo: String?
+    var autores = [String]()
+    var imagen: UIImage?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        search_txt.delegate = self
-        let uiButton = search_txt.valueForKey("cancelButton") as! UIButton
-        uiButton.setTitle("Clear", forState: UIControlState.Normal)
-        if esBusqueda == false{
-            search_txt.hidden=true
-            getData(isbn: self.ISBN!)
-        }
-      
-
-        self.title = ""
-        
         // Do any additional setup after loading the view.
+        
+        self.title_book.text = self.titulo!
+        var printauthors :String = ""
+        for autor in autores{
+            printauthors = printauthors + autor + "\n"
+        }
+        self.authors_book.text = printauthors
+        if imagen != nil {
+            imageURL.image = imagen!
+        }
+        
     }
     
     override func viewWillDisappear(animated: Bool) {
+        clear_txt()
     }
     
     override func viewWillAppear(animated: Bool) {
-        if self.esBusqueda
-        {
-            
-        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -61,113 +59,10 @@ class DetalleViewController: UIViewController, UISearchBarDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    func getData(isbn isbn:String){
-        let urls: String = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
-        //let urls: String = "10.10.10.10"
-        
-        //let urls: String = "http://dia.ccm.itesm.mx"
-        clear_txt()
-        let url = NSURL(string: urls + isbn)
-        let datos = NSData(contentsOfURL: url!)
-        
-        if datos == nil
-        {
-            let alert = UIAlertController(title: "Error conexión", message: "No hay conexión a internet o el servidor no está accesible.", preferredStyle: .Alert)
-            alert.addAction(UIAlertAction(title: "Aceptar", style: .Default, handler: nil))
-            self.presentViewController(alert, animated: true, completion: nil)
-        }
-        else
-        {
-            do
-            {
-                let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
-                let dico1 = json as! NSDictionary
-                if dico1["ISBN:"+isbn] != nil
-                {
-                    
-                    let dico2 = dico1["ISBN:"+isbn] as! NSDictionary
-                    let authors = dico2["authors"] as! NSArray
-                    //imprime el titulo de  el libro
-                    if dico2["title"] != nil
-                    {
-                        self.title_book.text =  dico2["title"] as! NSString as String
-                    }
-                    //imprime los autores de libro
-                    if authors.count > 0
-                    {
-                        var name: String?
-                        for author in authors
-                        {
-                            name = author["name"] as! NSString as String
-                            self.authors_book.text = "\(self.authors_book.text)- \(name!) \n"
-                        }
-                    }
-                    if dico2["cover"] != nil
-                    {
-                        let covers = dico2["cover"] as! NSDictionary
-                        var image_url : String?
-                        //muestra portada del de libro
-                        if covers["medium"] != nil
-                        {
-                            image_url = covers["medium"] as! NSString as String
-                        }
-                        else if covers["small"] != nil
-                        {
-                            image_url = covers["small"] as! NSString as String
-                        }
-                        else if covers["large"] != nil
-                        {
-                            image_url = covers["large"] as! NSString as String
-                        }
-                        if image_url != nil
-                        {
-                            let url_img = NSURL(string: image_url!)
-                            let data_img = NSData(contentsOfURL: url_img!)
-                            //make sure your image in this url does exist, otherwise unwrap in a if let check
-                            self.imageURL.image = UIImage(data: data_img!)
-                        }
-                    }
-                    if esBusqueda {
-                        delegate?.acceptDataFromDetalle([isbn , self.title_book.text])
-                    }
-                }
-                else
-                {
-                    let alert = UIAlertController(title: "ISBN Invalido", message: "No existe este libro en nuestras referencias.", preferredStyle: .Alert)
-                    alert.addAction(UIAlertAction(title: "Aceptar", style: .Default, handler: nil))
-                    self.presentViewController(alert, animated: true, completion: nil)
-                }
-            }
-            catch {
-                
-            }
-        }
-    }
-    
     func clear_txt(){
         self.title_book.text = ""
         self.authors_book.text = ""
         self.imageURL.image = nil
-    }
-    
-    func search(){
-        //sincrono(self.search_txt.text!)
-        getData(isbn: search_txt.text!)
-    }
-    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
-        search()
-        searchBar.resignFirstResponder() //desaparece el teclado
-    }
-    
-    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
-        search_txt.text = ""
-        clear_txt()
-    }
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        self.view.endEditing(true)
-        search()
-        return false
     }
 
     /*
--- buscadorLibrosTablas/buscadorLibrosTablas/TVC.swift
+++ buscadorLibrosTablas/buscadorLibrosTablas/TVC.swift
@@ -8,23 +8,77 @@
 
 import UIKit
 
-class TVC: UITableViewController, AcceptDataFromDetalleVC {
+import CoreData
+
+struct BookSearch {
+    var isbn : String
+  
+    var title : String
+    var authors : [String]
+    var image : UIImage?
     
-    private var libros : Array<Array<String>> = Array<Array<String>>()
-    
-    func acceptDataFromDetalle(data: Array<String>) {
-        self.libros.append(data)
+    init(isbn : String, title : String, authors : [String], image : UIImage? ){
+        self.isbn = isbn
+        self.title = title
+        self.authors = authors
+        if (image != nil){
+            self.image = image!
+        }
     }
+}
+
+class TVC: UITableViewController, UISearchBarDelegate {
     
-    override func viewWillAppear(animated: Bool) {
+    @IBOutlet weak var search_txt: UISearchBar!
+    
+    private var libros : Array<BookSearch> = Array<BookSearch>()
+    
+    var contexto : NSManagedObjectContext? = nil
+    
+    func addBockToCell(data: BookSearch) {
+        self.libros.append(data)
         tableView.reloadData()
     }
     
-
-
+    @IBAction func addBook(sender: UIBarButtonItem) {
+        search_txt.hidden = false
+    }
+    override func viewWillDisappear(animated: Bool) {
+        search_txt.hidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        search_txt.hidden = true
+        self.contexto = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
         self.title = "Buscador de Libros"
+        search_txt.delegate = self
+        let uiButton = search_txt.valueForKey("cancelButton") as! UIButton
+        uiButton.setTitle("Clear", forState: UIControlState.Normal)
+        let seccionEntidad = NSEntityDescription.entityForName("Libro", inManagedObjectContext: self.contexto!)
+        let peticion = seccionEntidad?.managedObjectModel.fetchRequestTemplateForName("petLibros")
+        do{
+           let seccionesEntidades = try self.contexto?.executeFetchRequest(peticion!)
+            for seccionEntidad2 in seccionesEntidades!{
+                let isbn = seccionEntidad2.valueForKey("isbn") as! String
+                let title = seccionEntidad2.valueForKey("title") as! String
+                var image : UIImage? = nil
+                if(seccionEntidad2.valueForKey("image") != nil){
+                    image = UIImage(data: seccionEntidad2.valueForKey("image") as! NSData)
+                }
+                
+                let autoresEntidad = seccionEntidad2.valueForKey("tiene") as! Set<NSObject>
+                var autores2 = [String]()
+                for autorEntidad2 in autoresEntidad{
+                    let auth = autorEntidad2.valueForKey("nombre") as! String
+                    autores2.append(auth)
+                }
+                let newBook = BookSearch.init(isbn: isbn, title: title, authors: autores2, image: image)
+                self.addBockToCell(newBook)
+            }
+        }
+        catch{
+        }
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -53,11 +107,155 @@ class TVC: UITableViewController, AcceptDataFromDetalleVC {
         let cell = tableView.dequeueReusableCellWithIdentifier("Celda", forIndexPath: indexPath)
 
         // Configure the cell...
-        cell.textLabel?.text = self.libros[indexPath.row][1]
-
+        cell.textLabel?.text = self.libros[indexPath.row].title
         return cell
     }
+    
+    func getData(isbn isbn:String)->BookSearch?{
+        let urls: String = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
+        //let urls: String = "10.10.10.10"
+        //let urls: String = "http://dia.ccm.itesm.mx"
+        let url = NSURL(string: urls + isbn)
+        let datos = NSData(contentsOfURL: url!)
+        
+        if datos == nil
+        {
+            let alert = UIAlertController(title: "Error conexión", message: "No hay conexión a internet o el servidor no está accesible.", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Aceptar", style: .Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+        else
+        {
+            do
+            {
+                let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                let dico1 = json as! NSDictionary
+                if dico1["ISBN:"+isbn] != nil
+                {
+                    let dico2 = dico1["ISBN:"+isbn] as! NSDictionary
+                    var new_title : String = ""
+                    var new_image : UIImage?
+                    var new_authors : [String] = [String]()
+                    var image_url : String?
+                   
+                    //carga el titulo de  el libro
+                    if dico2["title"] != nil
+                    {
+                        new_title =  dico2["title"] as! NSString as String
+                    }
+                    //carga los autores de libro
+                    if dico2["authors"] != nil {
+                        let authors = dico2["authors"] as! NSArray
+                        if authors.count > 0
+                        {
+                            for author in authors
+                            {
+                                new_authors.append( author["name"] as! NSString as String)
+                            }
+                        }
+                    }
+                   
+                    if dico2["cover"] != nil
+                    {
+                        let covers = dico2["cover"] as! NSDictionary
+                        //muestra portada del de libro
+                        if covers["medium"] != nil
+                        {
+                            image_url = covers["medium"] as! NSString as String
+                        }
+                        else if covers["small"] != nil
+                        {
+                            image_url = covers["small"] as! NSString as String
+                        }
+                        else if covers["large"] != nil
+                        {
+                            image_url = covers["large"] as! NSString as String
+                        }
+                        if image_url != nil
+                        {
+                            let url_img = NSURL(string: image_url!)
+                            let data_img = NSData(contentsOfURL: url_img!)
+                            //make sure your image in this url does exist, otherwise unwrap in a if let check
+                            //-----------
+                            new_image = UIImage(data: data_img!)
+                        }
+                    }
+                    let newBook = BookSearch.init(isbn: isbn, title: new_title, authors: new_authors, image: new_image)
+                    self.addBockToCell(newBook)
+                    return newBook
+                }
+                else
+                {
+                    let alert = UIAlertController(title: "ISBN Invalido", message: "No existe este libro en nuestras referencias.", preferredStyle: .Alert)
+                    alert.addAction(UIAlertAction(title: "Aceptar", style: .Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }
+            }
+            catch {
+            }
+        }
+        return nil
+    }
 
+    func crearAutoresEntidades(autores :[String]) -> Set<NSObject>{
+        var entidades = Set<NSObject>()
+        for autor in autores{
+            let autorEntidad =  NSEntityDescription.insertNewObjectForEntityForName("Autor", inManagedObjectContext: self.contexto!)
+            autorEntidad.setValue(autor, forKey: "nombre")
+            entidades.insert(autorEntidad)
+        }
+        return entidades
+    }
+    
+    func search(){
+        let seccionEntidad = NSEntityDescription.entityForName("Libro", inManagedObjectContext: self.contexto!)
+        let peticion = seccionEntidad?.managedObjectModel.fetchRequestFromTemplateWithName("petLibro", substitutionVariables: ["isbn": search_txt.text!])
+        do {
+            let seccionEntidad2 = try self.contexto?.executeFetchRequest(peticion!)
+            if seccionEntidad2?.count > 0 {
+                return
+            }
+        }
+        catch{
+        }
+        let seccion = getData(isbn: search_txt.text!)
+        if (seccion != nil){
+            let nuevaSeccionEntidad = NSEntityDescription.insertNewObjectForEntityForName("Libro", inManagedObjectContext: self.contexto!)
+            nuevaSeccionEntidad.setValue(seccion?.isbn, forKey: "isbn")
+            nuevaSeccionEntidad.setValue(seccion?.title, forKey: "title")
+            if seccion?.image != nil {
+                nuevaSeccionEntidad.setValue(UIImagePNGRepresentation((seccion?.image)!), forKey: "image")
+            }
+
+nuevaSeccionEntidad.setValue(crearAutoresEntidades((seccion?.authors)!), forKey: "tiene")
+            do {
+                try self.contexto?.save()
+                let detalleViewController = self.storyboard?.instantiateViewControllerWithIdentifier("DetalleViewController") as! DetalleViewController
+                detalleViewController.titulo = self.libros.last?.title
+                detalleViewController.autores = (self.libros.last?.authors)!
+                detalleViewController.imagen = self.libros.last?.image
+                self.navigationController?.pushViewController(detalleViewController, animated: true)
+            }
+            catch{
+            }
+        }
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        search()
+        searchBar.resignFirstResponder() //desaparece el teclado
+        search_txt.text = ""
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        search_txt.text = ""
+    }
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        search()
+        return false
+    }
 
     /*
     // Override to support conditional editing of the table view.
@@ -99,19 +297,14 @@ class TVC: UITableViewController, AcceptDataFromDetalleVC {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-
         let cl = segue.destinationViewController as! DetalleViewController
         let ip = self.tableView.indexPathForSelectedRow
-        if ip == nil {
-            cl.esBusqueda = true
+        if ip != nil {
+           cl.titulo = self.libros[ip!.row].title
+           cl.autores = self.libros[ip!.row].authors
+           cl.imagen = self.libros[ip!.row].image
         }
-        else{
-            cl.esBusqueda = false
-            //cl.titulo = self.libros[ip!.row][1]
-            cl.ISBN = self.libros[ip!.row][0]
-        }
-
-        cl.delegate = self
+  
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
